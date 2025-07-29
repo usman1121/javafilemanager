44b35e991b8804446d2b34ae4fdd8e5bb2cbd77c
omer <omarjafarcool@gmail.com>
2025-07-29 15:03:59 +0300

    fix the error

--- Menu/MenuManager.java
+++ Menu/MenuManager.java
@@ -1,12 +1,13 @@
 package Menu;
 
-import FileTasks.FileTaskHandler;
-import ActivityLog.Logger;
 import ActivityLog.LogViewer;
+import ActivityLog.Logger;
+import FileTasks.FileTaskHandler;
 import java.util.Scanner;
 
 public class MenuManager {
     private final FileTaskHandler fileTaskHandler = new FileTaskHandler();
+    @SuppressWarnings("unused")
     private final Logger logger = new Logger();
     private final LogViewer logViewer = new LogViewer();
     private final Scanner scanner = new Scanner(System.in);
@@ -23,53 +24,40 @@ public class MenuManager {
             System.out.println("7. View Logs");
             System.out.println("8. Move File/Directory");
             System.out.println("9. Navigate Directory");
+            System.out.println("10. Import File");
             System.out.println("0. Exit");
             System.out.print("Choose an option: ");
 
             String choice = scanner.nextLine();
             switch (choice) {
-                case "1":
+                case "1" -> {
                     String type = UserInput.getInput("Enter -f for file or -d for directory: ");
                     fileTaskHandler.createFile(type);
-                    break;
+                }
             
-                case "2":
-                    fileTaskHandler.deleteFile();
-                    break;
+                case "2" -> fileTaskHandler.deleteFile();
             
-                case "3":
-                    fileTaskHandler.renameFile();
-                    break;
+                case "3" -> fileTaskHandler.renameFile();
             
-                case "4":
-                    fileTaskHandler.listFiles();
-                    break;
-                case "5":
-                    fileTaskHandler.copyFile();
-                    break;
-
-                case "6":
-                    fileTaskHandler.searchFile();
-                    break;
-                case "7":
-                    logViewer.viewLogs();
-                    break;
+                case "4" -> fileTaskHandler.listFiles();
                     
-                case "8":
-                    fileTaskHandler.moveFile();
-                    break;
+                case "5" -> fileTaskHandler.copyFile();
+
+                case "6" -> fileTaskHandler.searchFile();
+                case "7" -> logViewer.viewLogs();
+
+                case "8" -> fileTaskHandler.moveFile();
             
-                case "9": 
-                    fileTaskHandler.navigatDiretory();
-                    break;
-        
+                case "9" -> fileTaskHandler.navigatDiretory();
+                    
+                case "10" -> fileTaskHandler.importFile();
             
-                case "0":
+                case "0" -> {
                     System.out.println("Goodbye!");
                     return;
+                }
             
-                default:
-                    System.out.println("Invalid choice. Try again.");
+                default -> System.out.println("Invalid choice. Try again.");
             }
             
         }
