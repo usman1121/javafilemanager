6279f26a19c013bdbeb0d1fad2177cac156a37f6
usman1121 <usmanabdella1121@gmail.com>
2025-07-29 20:49:27 +0300

    Updating Database making it better by adding error  error handling

--- Database/DatabaseConnection.java
+++ Database/DatabaseConnection.java
@@ -2,13 +2,24 @@ package Database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseConnection {
-    public static Connection getConnection() throws Exception {
-        String url = "jdbc:postgresql://localhost:5432/oopfinalproject";
-        String user = "postgres";
-        String password = "834927615"; 
+    private static final String URL = "jdbc:postgresql://localhost:5432/oopfinalproject";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "834927615"; 
 
-        return DriverManager.getConnection(url, user, password);
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.err.println("Database connection failed: " + e.getMessage());
+            return null;
+        } catch (ClassNotFoundException e) {
+            System.err.println("PostgreSQL Driver not found. Did you add the JAR?");
+            return null;
+        }
     }
 }
